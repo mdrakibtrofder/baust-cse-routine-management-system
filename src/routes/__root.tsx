d78eaaa244f936897ed02a60a9d4b0adf009a234
Md. Rakib Trofder <mdrakibtrofder@gmail.com>
2026-07-05 15:19:32 +0600

    Add favicon in routine management

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -40,6 +40,11 @@ export const Route = createRootRoute({
         rel: "stylesheet",
         href: appCss,
       },
+      {
+        rel: "icon",
+        type: "image/svg+xml",
+        href: "/favicon.svg",
+      },
     ],
   }),
   shellComponent: RootShell,
